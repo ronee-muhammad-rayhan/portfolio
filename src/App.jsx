933e79d685ee25f0ea45376b79d0f3509f7b01af
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-12-12 19:23:02 +0600

    projects section added

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,17 @@ function App() {
     <>
       <div id='home'>
         {/* header section */}
-        <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
+        <header className="p-4 bg-gray-100 dark:bg-gray-800 dark:text-gray-100">
           <div className="container flex justify-between h-16 mx-auto relative">
             <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-8 h-8 dark:text-violet-400">
-                <path d="M27.912 7.289l-10.324-5.961c-0.455-0.268-1.002-0.425-1.588-0.425s-1.133 0.158-1.604 0.433l0.015-0.008-10.324 5.961c-0.955 0.561-1.586 1.582-1.588 2.75v11.922c0.002 1.168 0.635 2.189 1.574 2.742l0.016 0.008 10.322 5.961c0.455 0.267 1.004 0.425 1.59 0.425 0.584 0 1.131-0.158 1.602-0.433l-0.014 0.008 10.322-5.961c0.955-0.561 1.586-1.582 1.588-2.75v-11.922c-0.002-1.168-0.633-2.189-1.573-2.742zM27.383 21.961c0 0.389-0.211 0.73-0.526 0.914l-0.004 0.002-10.324 5.961c-0.152 0.088-0.334 0.142-0.53 0.142s-0.377-0.053-0.535-0.145l0.005 0.002-10.324-5.961c-0.319-0.186-0.529-0.527-0.529-0.916v-11.922c0-0.389 0.211-0.73 0.526-0.914l0.004-0.002 10.324-5.961c0.152-0.090 0.334-0.143 0.53-0.143s0.377 0.053 0.535 0.144l-0.006-0.002 10.324 5.961c0.319 0.185 0.529 0.527 0.529 0.916z"></path>
-                <path d="M22.094 19.451h-0.758c-0.188 0-0.363 0.049-0.515 0.135l0.006-0.004-4.574 2.512-5.282-3.049v-6.082l5.282-3.051 4.576 2.504c0.146 0.082 0.323 0.131 0.508 0.131h0.758c0.293 0 0.529-0.239 0.529-0.531v-0.716c0-0.2-0.11-0.373-0.271-0.463l-0.004-0.002-5.078-2.777c-0.293-0.164-0.645-0.26-1.015-0.26-0.39 0-0.756 0.106-1.070 0.289l0.010-0.006-5.281 3.049c-0.636 0.375-1.056 1.055-1.059 1.834v6.082c0 0.779 0.422 1.461 1.049 1.828l0.009 0.006 5.281 3.049c0.305 0.178 0.67 0.284 1.061 0.284 0.373 0 0.723-0.098 1.027-0.265l-0.012 0.006 5.080-2.787c0.166-0.091 0.276-0.265 0.276-0.465v-0.716c0-0.293-0.238-0.529-0.529-0.529z"></path>
-              </svg>
+              <img className='rounded-full w-10' src="../public/assets/images/profile.png" alt="profile" />
             </a>
             <ul className="items-stretch hidden space-x-3 lg:flex">
               <li className="flex">
                 <a rel="noopener noreferrer" href="#home" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">Home</a>
               </li>
               <li className="flex">
-                <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent text-white">About</a>
+                <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent /*  text-white */">About</a>
               </li>
               <li className="flex">
                 <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Skills</a>
@@ -88,6 +85,10 @@ function App() {
       {/* main part */}
       <main>
         {/* banner section */}
+        <section className='w-full mx-auto bg-red-300'>
+          <img className='object-cover h-[600px] w-full' src="../public/assets/images/banner.jpg" alt="banner" />
+          {/* <img className='' src="../public/assets/images/banner.avif" alt="banner" width="700px" height="300px" style={{ width: "700px", height: "300px" }} /> */}
+        </section>
         {/* about section */}
         <section>
           <h3 className="text-4xl text-red-600 font-bold text-center py-10">About</h3>
@@ -124,10 +125,43 @@ function App() {
           <br />
         </section>
         {/* experiences section */}
+        <section>
+          <h3 className="text-4xl text-red-600 font-bold text-center">Experiences</h3>
+          <p>I have 1 Year Experience in Web Development especially in frontend focusing on MERN Stack</p>
+        </section>
         {/* projects section */}
+        <section>
+          <h3 className="text-4xl text-red-600 font-bold text-center">Projects</h3>
+          <div className="bg-gray-100 text-gray-800">
+            <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+              <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                <img src="../public/assets/images/tour.png" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+              </div>
+              <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+                <h1 className="text-5xl font-bold leadi sm:text-6xl">Tourist Guide
+                  {/* <span className="text-indigo-600">senectus</span>erat pharetra */}
+                </h1>
+                <p className="mt-6 mb-8 text-lg sm:mb-12">Tourist Guide is a fully functional website using MERN Stack.
+                  <br className="hidden md:inline lg:hidden" />There are 3 types role 1.tourist, 2.tour-guide and 3.admin.
+                </p>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded hover:text-red-500 bg-indigo-600 text-gray-50">Client</a>
+                  <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded hover:text-red-500 hover:bg-indigo-600 border-gray-800">Live</a>
+                  <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded  hover:text-red-500 bg-indigo-600 text-gray-50">Server</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
         {/* education section */}
+        <section className='py-10'>
+          <h3 className="text-4xl text-red-600 font-bold text-center">Education</h3>
+          <p>I have completed PGD-in-ICT in 2016</p>
+          <p>I have completed B.Sc. Engineering in 2015</p>
+          <p></p>
+        </section>
         {/* contact information section */}
-        <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
+        <section className="py-6 bg-gray-100 dark:bg-gray-800 dark:text-gray-50">
           <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
             <div className="py-6 md:py-0 md:px-6">
               <h1 className="text-4xl font-bold">Get in touch</h1>
@@ -137,20 +171,20 @@ function App() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
                   </svg>
-                  <span>Fake address, 9999 City</span>
+                  <span>Rangpur, Bangladesh</span>
                 </p>
                 <p className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                   </svg>
-                  <span>123456789</span>
+                  <span>+880 1737716972</span>
                 </p>
                 <p className="flex items-center">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                     <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                   </svg>
-                  <span>contact@business.com</span>
+                  <span>roneemrayhan.edu@gmail.com</span>
                 </p>
               </div>
             </div>
@@ -167,7 +201,7 @@ function App() {
                 <span className="mb-1">Message</span>
                 <textarea rows="3" className="block w-full rounded-md focus:ring focus:ri focus:ri dark:bg-gray-800" spellCheck="false" data-ms-editor="true"></textarea>
               </label>
-              <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ri dark:bg-violet-400 dark:text-gray-900 focus:ri hover:ri">Submit</button>
+              <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ri bg-gray-50 dark:bg-violet-400 dark:text-gray-900 focus:ri hover:ri">Submit</button>
             </form>
           </div>
         </section>
@@ -175,14 +209,14 @@ function App() {
       </main>
 
       {/* footer section */}
-      <footer className="dark:bg-gray-800 dark:text-gray-50">
+      <footer className="bg-gray-200 dark:bg-gray-800 dark:text-gray-50">
         <div className="container flex flex-col p-4 mx-auto md:p-8 lg:flex-row dark:divide-gray-400">
           <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
-            <li>Shop</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Pricing</li>
-            <li>Contact</li>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#home'>About</a></li>
+            <li><a href='#home'>Skills</a></li>
+            <li><a href='#home'>Projects</a></li>
+            <li><a href='#home'>Contact</a></li>
           </ul>
           <div className="flex flex-col justify-center pt-6 lg:pt-0">
             <div className="flex justify-center space-x-4">
